50c3fdccec69bc958dc6b50014de131f14794bf7
SkyrukCS <skyruk.cs@gmail.com>
2024-04-25 22:06:18 -0400

    First Commit basic website setup

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+import InstagramIcon from '@mui/icons-material/Instagram';
+import TikTokIcon from '@mui/icons-material/LibraryMusic';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import "../styles/Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div className="socialMedia">
+        <InstagramIcon /> <TikTokIcon /> <GitHubIcon />
+      </div>
+      <p> &copy; 2024 skyrukcs.com</p>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ src/pages/Home.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import BannerImage from "../assets/BannerImage.png";
+import "../styles/Home.css";
+
+function Home() {
+  return (
+    <div className="home" style={{ backgroundImage: `url(${BannerImage})` }}>
+      <div className="headerContainer">
+        <h1> Skyruk Creative Studios </h1>
+        <p> PROJECTS MADE FOR FUN! </p>
+        <Link to="/hall">
+          <button> Explore Now </button>
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
