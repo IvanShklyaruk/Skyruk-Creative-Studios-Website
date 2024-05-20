fa7c82266f1229ceb2fdd78c4127fbf93e3ce40e
IvanShklyaruk <shklyaruk.ivan@gmail.com>
2024-05-20 14:18:45 -0400

    Setup and ready for the library model import

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -4,6 +4,10 @@ export const Experience = () => {
     return (
         <>
             <OrbitControls />
+            <mesh>
+                <boxGeometry />
+                <meshNormalMaterial />
+            </mesh>
         </>
     )
 }
--- src/pages/GamesPage.jsx
+++ src/pages/GamesPage.jsx
@@ -16,7 +16,7 @@ const GamesPage = () => {
             <video autoPlay loop muted playsInline className="background-video" key={GamesPageBackground}>
                 <source src={GamesPageBackground} type="video/mp4" />
             </video>
-            <button onClick={handleLibraryClick} className="interactive-area2" style={{ top: '13%', left: '9.5%', width: '41%', height: '16%' }}>Riddler</button>
+            <button onClick={handleLibraryClick} className="interactive-area2" style={{ top: '13%', left: '9.5%', width: '41%', height: '16%' }}>Wordle</button>
             <button className="interactive-area2" style={{ top: '13%', left: '51.5%', width: '39.5%', height: '16%' }}>Coming Soon...</button>
             <button className="interactive-area2" style={{ top: '32%', left: '9.5%', width: '41%', height: '16%' }}>Coming Soon...</button>
             <button className="interactive-area2" style={{ top: '32%', left: '51.5%', width: '39.5%', height: '16%' }}>Coming Soon...</button>
