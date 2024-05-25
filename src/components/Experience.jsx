b80f417b10f22afbefe06d71159482f6ae7c938f
IvanShklyaruk <shklyaruk.ivan@gmail.com>
2024-05-25 00:11:10 -0400

    Started working on the camera animation

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,15 +1,28 @@
-import { OrbitControls } from "@react-three/drei"
-import { LibraryModel } from "./LibraryModel"
-import CameraPositionLogger from "../helpers/CameraPositionLogger"
+import {
+  OrbitControls,
+  CameraControls,
+  Environment,
+  Gltf,
+} from "@react-three/drei";
+import { useRef } from "react";
+import { button, useControls } from "leva";
+import { LibraryModel } from "./LibraryModel";
+import CameraPositionLogger from "../helpers/CameraPositionLogger";
 
 export const Experience = () => {
-    return (
-        <>
-            <ambientLight intensity={1} />
-            <OrbitControls />
-            <CameraPositionLogger event='mousedown' />
-            <pointLight position={[-5, 3, 0]} intensity={100}/>
-            <LibraryModel />
-        </>
-    )
-}
+  const controls = useRef();
+
+  useControls("Dolly", {
+    in: button(() => controls.current.dolly(1, true)),
+    out: button(() => controls.current.dolly(-1, true)),
+  });
+  return (
+    <>
+      <CameraControls ref={controls} />
+      <ambientLight intensity={1} />
+      <CameraPositionLogger event="mousedown" />
+      <pointLight position={[-5, 3, 0]} intensity={100} />
+      <LibraryModel />
+    </>
+  );
+};
