5008db8ca1038a44f41a3a39e8db9ee3326a4c37
IvanShklyaruk <shklyaruk.ivan@gmail.com>
2024-06-07 08:52:09 -0400

    UI Fixes to scale with screen size

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,38 +1,21 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
 import Logo from "../assets/SkyrukLogo.png"; // Ensure the logo path is correct
-import ReorderIcon from "@mui/icons-material/Reorder";
+import { Link } from "react-router-dom";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import TikTokIcon from "@mui/icons-material/LibraryMusic";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import "../styles/Navbar.css";
 
 const Navbar = () => {
-  const [openLinks, setOpenLinks] = useState(false);
-
-  const toggleNavbar = () => {
-    setOpenLinks(!openLinks);
-  };
-
   return (
     <div className="navbar">
-      <div className="leftSide" id={openLinks ? "open" : "close"}>
-        <Link to="/">
-          <img src={Logo} alt="Logo" />
-        </Link>
-        <div className="hiddenLinks">
-          <Link to="/">Home</Link>
-          <Link to="/library">Library</Link>
-          <Link to="/about">About</Link>
-        </div>
+      <div className="leftSide">
+        <img src={Logo} alt="Logo" />
       </div>
       <div className="rightSide">
         <Link to="/">Home</Link>
         <Link to="/library">Library</Link>
         <Link to="/about">About</Link>
-        <button onClick={toggleNavbar}>
-          <ReorderIcon />
-        </button>
         <div className="socialIcons">
           <InstagramIcon />
           <TikTokIcon />
