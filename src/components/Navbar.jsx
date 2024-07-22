93c8b15ac9be249f9756968435989b9647cc9977
IvanShklyaruk <shklyaruk.ivan@gmail.com>
2024-07-22 16:20:55 -0400

    3d and 2d library toggle added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,15 +2,23 @@ import React from "react";
 import Logo from "../assets/SkyrukLogo.png";
 import { Link } from "react-router-dom";
 import GitHubIcon from "@mui/icons-material/GitHub";
+import Switch from "@mui/material/Switch";
 import "../styles/Navbar.css";
 
-const Navbar = () => {
+const Navbar = ({ showSlider, is3D, toggleView }) => {
   return (
     <div className="navbar">
       <div className="leftSide">
         <Link to="/">
           <img src={Logo} alt="Logo" />
         </Link>
+        {showSlider && (
+          <div className="viewToggle">
+            <span className="viewLabel">2D</span>
+            <Switch checked={is3D} onChange={toggleView} />
+            <span className="viewLabel">3D</span>
+          </div>
+        )}
       </div>
       <div className="rightSide">
         <Link to="/">Home</Link>
